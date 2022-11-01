4b7bafa29c6f00fcb8f7df7e13d99f5a418c6aec
DIEGO FEDER <63731464+diegovfeder@users.noreply.github.com>
2022-11-01 02:52:37 -0300

    WIP

---
+++ mobile/src/screens/SignIn.tsx
@@ -0,0 +1,13 @@
+import { Center, Text } from "native-base";
+
+const SignIn = () => {
+  return (
+    <Center flex={1} bgColor={"gray.900"}>
+      <Text color={"white"} fontSize={24} fontFamily="heading">
+        Sign In
+      </Text>
+    </Center>
+  );
+};
+
+export default SignIn;
